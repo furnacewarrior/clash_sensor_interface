70745413eb994891fa4d3401531e06069b9b80c9
Bart Wijlens <bartwijlens@gmail.com>
2018-10-04 12:58:23 +0200

    rework state diagram

--- Modules/Interfaces/SPI/Designs/StreamDelay.hs
+++ Modules/Interfaces/SPI/Designs/StreamDelay.hs
@@ -3,8 +3,10 @@ import Clash.Explicit.Prelude
 import ClashAddon
 import ConnectorsStd
  
-data CtrlState = Init | Process | ProcessEnd | HoldStart | HoldStartS | Hold | HoldS | HoldEnd
+data CtrlState = Init | ReadWrite | ReadWrite# | WriteData | WriteData# | Hold | Hold2 | Hold# | ReadData | HoldPreEnd
 
+-- # means the busy signal is reset here
+-- number means same state but has different name for state machine
 version = "1.0"
 ctrl :: (KnownNat a, c ~ (Unsigned 16, Unsigned 16, (BitVector a, Bit), Bit, BitVector a, Bit, Bit, Bit, CtrlState))
   => (Unsigned 16, Unsigned 16)
@@ -30,60 +32,60 @@ ctrl (dataSize, delay) state input = state'
       _ -> stateM_4
 
     stateM_1 = case stateL of
-      Init -> HoldEnd
-      Process -> case counter < (dataSize - 2) of
-                  True -> Process
-                  False -> ProcessEnd
-      ProcessEnd -> HoldStart
-      HoldStart -> HoldEnd  
-      HoldEnd -> Process
+      Init -> ReadData
+      ReadWrite -> case counter < (dataSize - 2) of
+                  True -> ReadWrite
+                  False -> ReadWrite#
+      ReadWrite# -> WriteData
+      WriteData -> ReadData  
+      ReadData -> ReadWrite
       _ -> Init
  
     stateM_2 = case stateL of
-      Init -> HoldEnd
-      Process -> case counter < (dataSize - 1) of
-                   True -> Process
-                   False -> HoldStartS
-      HoldStartS -> Hold
-      Hold -> HoldEnd  
-      HoldEnd -> Process
+      Init -> ReadData
+      ReadWrite -> case counter < (dataSize - 1) of
+                   True -> ReadWrite
+                   False -> WriteData#
+      WriteData# -> Hold
+      Hold -> ReadData  
+      ReadData -> ReadWrite
       _ -> Init
 
     stateM_3 = case stateL of
-      Init -> HoldEnd
-      Process -> case counter < (dataSize - 1) of
-                   True -> Process
-                   False -> HoldStart
-      HoldStart -> HoldS
-      HoldS -> Hold
-      Hold -> HoldEnd
-      HoldEnd -> Process
+      Init -> ReadData
+      ReadWrite -> case counter < (dataSize - 1) of
+                   True -> ReadWrite
+                   False -> WriteData
+      WriteData -> Hold#
+      Hold# -> Hold
+      Hold -> ReadData
+      ReadData -> ReadWrite
       _ -> Init
 
     stateM_4 = case stateL of
-      Init -> HoldEnd
-      Process -> case counter < (dataSize - 1) of
-                   True -> Process
-                   False -> HoldStart
-      HoldStart -> Hold
+      Init -> ReadData
+      ReadWrite -> case counter < (dataSize - 1) of
+                   True -> ReadWrite
+                   False -> WriteData
+      WriteData -> Hold
       Hold -> case (counterDelay == (delay - 3)) of
-        True -> HoldS
-        False -> case (counterDelay < delay) of 
-          True -> Hold
-          False -> HoldEnd
-      HoldEnd -> Process
+        True -> Hold#
+        False -> Hold
+      Hold# -> Hold2
+	  Hold2 -> ReadData
+      ReadData -> ReadWrite
       _ -> Init
 
     state' = case (stateNext) of
---                 (    counter,     counterDelay,            (buffer, mosi),   cs,                dataOut,   NO, clkO,busyO,   stateL)
-      Init      -> (          0,                0, shiftBitInOut buffer miso, high,                dataOut,  low,  low, high, stateNext)
-      Process   -> (counter + 1,                0, shiftBitInOut buffer miso,  low,                dataOut,  low, high, high, stateNext)
-      ProcessEnd-> (          0,                0, shiftBitInOut buffer miso,  low,                dataOut,  low, high,  low, stateNext)
-      HoldStart -> (          0, counterDelay + 1, shiftBitInOut buffer miso,  low, shiftBitIn buffer miso, high,  low, high, stateNext)
-      HoldStartS-> (          0, counterDelay + 1, shiftBitInOut buffer miso,  low, shiftBitIn buffer miso, high,  low,  low, stateNext)
-      Hold      -> (          0, counterDelay + 1, shiftBitInOut buffer miso,  low,                dataOut,  low,  low, high, stateNext)
-      HoldS      ->(          0, counterDelay + 1, shiftBitInOut buffer miso,  low,                dataOut,  low,  low,  low, stateNext)
-      HoldEnd   -> (          0,                0, shiftBitInOut dataIn miso,  low,                dataOut,  low, high, high, stateNext)
+--                   (    counter,     counterDelay,            (buffer, mosi),   cs,                dataOut,   NO, clkO,busyO,   stateL)
+      Init        -> (          0,                0, shiftBitInOut buffer miso, high,                dataOut,  low,  low, high, stateNext)
+      ReadWrite   -> (counter + 1,                0, shiftBitInOut buffer miso,  low,                dataOut,  low, high, high, stateNext)
+      ReadWrite#  -> (          0,                0, shiftBitInOut buffer miso,  low,                dataOut,  low, high,  low, stateNext)
+      WriteData   -> (          0,                0, shiftBitInOut buffer miso,  low, shiftBitIn buffer miso, high,  low, high, stateNext)
+      WriteData#  -> (          0,                0, shiftBitInOut buffer miso,  low, shiftBitIn buffer miso, high,  low,  low, stateNext)
+      Hold, Hold2 -> (          0, counterDelay + 1, shiftBitInOut buffer miso,  low,                dataOut,  low,  low, high, stateNext)
+      Hold#       -> (          0,                0, shiftBitInOut buffer miso,  low,                dataOut,  low,  low,  low, stateNext)
+      ReadData    -> (          0,                0, shiftBitInOut dataIn miso,  low,                dataOut,  low, high, high, stateNext)
 
 ctrlOut state = output
   where
